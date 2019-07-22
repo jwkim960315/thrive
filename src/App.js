21634bb8fce45d096370cf20aa4ba0c5a3556d65
jwkim960315 <jkim11@wesleyan.edu>
2019-07-23 01:20:30 +0900

    playing with konvajs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import { PaperContainer, Circle, Layer } from '@psychobolt/react-paperjs'
+import React, { Component } from 'react';
+import Konva from 'konva';
+import { render } from 'react-dom';
+import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
+import './App.css';
 
-const Shapes = () => <Circle center={[120, 50]} radius={35} fillColor="#00FF00" />;
 
-const App = props => (
-    <div>
-      <PaperContainer {...props}>
-        <Circle center={[80, 50]} radius={35} fillColor="red" />
-        <Layer>
-          <Shapes />
-        </Layer>
-      </PaperContainer>
-    </div>
-);
+class App extends Component {
+    state = {
+        screenCenter: [window.innerWidth/2,window.innerHeight/2],
+        circleFill: 'white',
+        circleStroke: '#49d3fc'
+    }
+
+    updateDimension = () => {
+        this.setState({ screenCenter: [window.innerWidth/2,window.innerHeight/2] })
+    }
+
+    handleMouseEnter = e => {
+        this.setState({
+            circleFill: '#49d3fc',
+            // circleStroke: 'white'
+        });
+    }
+
+    handleMouseLeave = e => {
+        this.setState({
+            circleFill: 'white',
+            // circleStroke: '#49d3fc'
+        });
+    }
+
+    /**
+     * Add event listener
+     */
+    componentDidMount() {
+        window.addEventListener("resize", this.updateDimension);
+    }
+
+    /**
+     * Remove event listener
+     */
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateDimension);
+    }
+
+    render() {
+        return (
+            <Stage width={window.innerWidth} height={window.innerHeight}>
+                <Layer>
+                    <Circle
+                        x={this.state.screenCenter[0]}
+                        y={this.state.screenCenter[1]}
+                        radius={100}
+                        stroke={this.state.circleStroke}
+                        fill={this.state.circleFill}
+                        strokeWidth={4}
+                        onMouseEnter={this.handleMouseEnter}
+                        onMouseLeave={this.handleMouseLeave}
+                    />
+                </Layer>
+            </Stage>
+        );
+    }
+}
+
 
 export default App;
